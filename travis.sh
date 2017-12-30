f5997ecfe9ad87db04e651cb0ef97e55c599df92
Kaido <kaido@myblossom.com>
2017-12-29 20:38:39 -0800

    Move code from travis to shell script

--- travis.sh
+++ travis.sh
@@ -9,9 +9,25 @@ source keychain.sh
 source build.sh
 source upload.sh
 
+run_build() {
 decrypt_files
 build_keychain
 main_build
 
 upload_ios
 upload_android
+}
+
+prepare_artifacts() {
+# clean out decrypted files
+rm -rf certs/ profile/
+mkdir -p build_artifacts
+#package up the whole build dir
+tar czf /tmp/build.travis-$TRAVIS_JOB_NUMBER-job.tgz $TRAVIS_BUILD_DIR
+mv /tmp/build.travis-$TRAVIS_JOB_NUMBER-job.tgz build_artifacts
+#copy key config files
+cp -v build/config.xml build/package.json build/ionic.config.json build/package-lock.json build_artifacts
+#build products
+cp -v build/*.ipa build/*.apk build_artifacts
+tar cvzf build_artifacts/ipa.travis-$TRAVIS_JOB_NUMBER-job.tgz build/*.ipa build/*.apk
+}

1f7e8b3a75893aa2ba56998f33f1c49c9c78b7b5
Kaido <kaido@myblossom.com>
2017-12-30 22:44:53 -0800

    Fix upload once again

--- upload.sh
+++ upload.sh
@@ -5,18 +5,20 @@ if [[ -z "$HOCKEYAPP_TOKEN" ]]; then
     return
 fi
 
+if [ -z "TRAVIS_COMMIT_MESSAGE" ] ; then
+  MSG="Auto upload";
+else
+  MSG=$TRAVIS_COMMIT_MESSAGE
+fi
+
 upload_ios() {
 TARGET=$APP_SHORT.adhoc.ipa
 if [ ! -s $TARGET ]; then
   echo "Adhoc distribution file doesnt exist:" $TARGET
   return
 fi
-if [ -z "TRAVIS_COMMIT_MESSAGE" ] ; then
-  MSG="Auto upload";
-else
-  MSG=$TRAVIS_COMMIT_MESSAGE
-fi
 echo "Uploading " $TARGET
+set -x
 curl  -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
   -F status=2 \
   -F notify=0 \
@@ -26,6 +28,7 @@ curl  -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
   -F "ipa=@$TARGET" \
   -F tags=ci \
   https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID_IOS/app_versions/upload
+set +x
 }
 upload_android() {
 TARGET=app-release.apk
@@ -33,6 +36,7 @@ if [ ! -s $TARGET ]; then
   echo "Rel apk file doesnt exist:" $TARGET
   return
 fi
+set -x
 curl  -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
   -F status=2 \
   -F notify=0 \
@@ -41,5 +45,6 @@ curl  -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
   -F strategy=replace \
   -F "ipa=@$TARGET" \
   -F tags=ci \
-  https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID_ANDROID/
+  https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID_ANDROID/app_versions/upload
+set +x
 }
